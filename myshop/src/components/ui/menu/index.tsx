58efe4f9b091bb7cb08644f449bf7c1971108a9c
ScottWombat <scottwombat@gmail.com>
2024-06-07 13:43:27 +1000

    update menu

--- myshop/src/components/ui/menu/index.tsx
+++ myshop/src/components/ui/menu/index.tsx
@@ -1,39 +1,39 @@
-import React, { useState,useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
-import styles from  './menu.module.css';
+import styles from './menu.module.css';
 import './slide.module.css';
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 /*
 https://codepen.io/Winterfox/pen/wGmEbY
 */
-const Submenu =(props) =>{
- 
+const Submenu = (props) => {
+
   return (
     
     <ul className={styles.nav__submenu}>
-    <li className={styles.nav__submenu_item}>
-      <a onClick={ (e) => props.clickHandler(e,props.catalog, props.category)}>Our Company</a>
-    </li>
-    <li className={styles.nav__submenu_item}>
-      <a>Our Team</a>
-    </li>
-    <li className={styles.nav__submenu_item}>
-      <a>Our Portfolio</a>
-    </li>
-  </ul>
+      <li className={styles.nav__submenu_item}>
+        <a onClick={(e) => props.clickHandler(e, props.catalog, props.category)}>Our Company</a>
+      </li>
+      <li className={styles.nav__submenu_item}>
+        <a>Our Team</a>
+      </li>
+      <li className={styles.nav__submenu_item}>
+        <a>Our Portfolio</a>
+      </li>
+    </ul>
 
   );
 }
 
 const Menu = (props) => {
   const componentRef = useRef(null);
-  let navigate = useNavigate(); 
+  let navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(true);
-  useEffect(()=>{
-   
-    },[])
-  const onClick = (e ,catalog,category) => {
+  useEffect(() => {
+
+  }, [])
+  const onClick = (e, catalog, category) => {
     e.preventDefault();
     setShowMenu(false);
     navigate('/content?catalog=Men;category=CockRings');
@@ -41,41 +41,55 @@ const Menu = (props) => {
   const handleHover = (event) => {
     setShowMenu(true)
   };
-  
- const handleLeave = (event) => {
-    setShowMenu(false);
-   
-  };
 
-  return(
-    <nav className={styles.nav}>
-    <ul className={styles.nav__menu}>
-      <li className={styles.nav__menu_item}>
-        <a>Home</a>
-      </li>
-      <li className={styles.nav__menu_item} onMouseLeave={ (e) => handleLeave(e)}>
-        <a onMouseEnter={(e) => handleHover(e)}>
-          For him
-        </a>
-        {showMenu &&
-        <div className={styles.submenu_container}>
-          <CSSTransition classNames="slide"  timeout={300}>
-            <Submenu clickHandler={onClick} catalog="Men" category='CockRings' /> 
-          </CSSTransition>
+  const handleLeave = (event) => {
+    setShowMenu(false);
+
+  };
+  /*
+<ul className={styles.menu}>
+          <li><a href="#home">FOR HIM</a></li>
+          <li><a href="#news">FOR HER</a></li>
+          <li><a href="#contact">SALE</a></li>
+          <li><a href="#about">NEW ARRIVALS</a></li>
+          </ul>
+  */
+
+  return (
+    <nav>
+  <ul className={styles.container + ' ' + styles.ul_reset}>
+
+    <li className={styles.droppable}>
+      <a href='#'>FOR HIM</a>
+      <div className={styles.mega_menu}>
+      	<div className={styles.container1 + ' ' + styles.ul_reset}>
+          <ul className={styles.ul_reset}>
+            <div className={styles.header}>Toys for your penis</div>
+            <li><a href='#'>Cock Rings</a></li>
+            <li><a href='#'>Masturbators</a></li>
+            <li><a href='#'>Penis Extenders</a></li>
+            <li><a href='#'>Penis Pumps</a></li>
+            <li><a href='#'>Flashlights</a></li>
+          </ul>
+          <ul className={styles.ul_reset}>
+            <div className={styles.header}>Toys for your penis</div>
+            <li><a href='#'>Cock Rings</a></li>
+            <li><a href='#'>Masturbators</a></li>
+            <li><a href='#'>Penis Extenders</a></li>
+            <li><a href='#'>Penis Pumps</a></li>
+            <li><a href='#'>Flashlights</a></li>
+          </ul>
         </div>
-        }
-      </li>
-      <li className={styles.nav__menu_item}>
-        <a>For her</a>
-      </li>
-      <li className={styles.nav__menu_item}>
-        <a>Sale</a>
-      </li>
-      <li className={styles.nav__menu_item}>
-        <a>New Arrivals</a>
-      </li>
-    </ul>
-  </nav>
+      </div>
+    </li>
+   
+    <li><a href='#'>HER TOYS</a></li>
+    <li><a href='#'>HIS TOYS</a></li>
+    <li><a href='#'>COUPLE TOYS</a></li>
+    <li><a href='#'>BEST SELLERS</a></li>
+    <li><a href='#'>SALE</a></li>
+  </ul>
+</nav>
   );
 
 }
